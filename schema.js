a32de723f86a1600eb39449455c441b2ebe08aa8
Kyra Edwards <kedward8@oswego.edu>
2025-04-30 13:45:24 -0400

    added types and imported it to schema

--- schema.js
+++ schema.js
@@ -1,1 +1,25 @@
-fff
+export const typeDefs = `#graphql
+type Game {
+  id: ID!
+  title: String!
+  platform: [String!]!
+}
+
+type Review {
+    id: ID!
+    rating: int!
+    content: String!
+}
+
+type Author {
+    id: ID!
+    name: String!
+    verified: boolean
+}
+
+type Query{
+    reviews: [Review]
+    games: [Game]
+    authors: [Author]
+}
+`;
