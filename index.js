adfbdc5fdbd3a8873c65a8f9c6917f92259abdea
Kyra Edwards <kedward8@oswego.edu>
2025-05-02 13:06:59 -0400

    made changes

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 import { ApolloServer } from "@apollo/server"
 import { startStandaloneServer } from "@apollo/server/standalone"
 import { typeDefs } from "./schema"
+import db from './_db.js'
+
+const resolvers = {
+  Query: {
+    games(){
+      return db.games
+    }, 
+    reviews(){
+      return db.reviews
+    }, 
+    authors(){
+      return db.authors
+    }
+  }
+}
 
 const server = new ApolloServer({
-  typeDefs
+  typeDefs,
+  resolvers
 })
 
 const {url} = await startStandaloneServer(server, {
